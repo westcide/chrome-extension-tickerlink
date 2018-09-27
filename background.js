4bbed283d94f395e7f5be53f0e7fcaf6a4827d0f
nitchie <nitchie@westcide.com>
2018-09-27 17:52:22 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,52 @@
+// Copyright 2018 westcide.com All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+// Add a listener to create the initial context menu items,
+// context menu items only need to be created at runtime.onInstalled
+chrome.runtime.onInstalled.addListener(function() {
+  for (let key of Object.keys(kSites)) {
+    chrome.contextMenus.create({
+      id: key,
+      title: kSites[key],
+      type: 'normal',
+      contexts: ['selection'],
+    });
+  }
+});
+
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+  let url =
+//    'https://google.' + item.menuItemId + '/search?q=' + item.selectionText;
+    item.menuItemId.replace(/\<CODE\>/,item.selectionText);
+  chrome.tabs.create({url: url, index: tab.index + 1});
+});
+
+chrome.storage.onChanged.addListener(function(list, sync) {
+  let newlyDisabled = [];
+  let newlyEnabled = [];
+  let currentRemoved = list.removedContextMenu.newValue;
+  let oldRemoved = list.removedContextMenu.oldValue || [];
+  for (let key of Object.keys(kSites)) {
+    if (currentRemoved.includes(key) && !oldRemoved.includes(key)) {
+      newlyDisabled.push(key);
+    } else if (oldRemoved.includes(key) && !currentRemoved.includes(key)) {
+      newlyEnabled.push({
+        id: key,
+        title: kSites[key]
+      });
+    }
+  }
+  for (let locale of newlyEnabled) {
+    chrome.contextMenus.create({
+      id: locale.id,
+      title: locale.title,
+      type: 'normal',
+      contexts: ['selection'],
+    });
+  }
+  for (let locale of newlyDisabled) {
+    chrome.contextMenus.remove(locale);
+  }
+});
---
+++ sites.js
@@ -0,0 +1,13 @@
+// Copyright 2018 westcide.com All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const kSites = {
+  'https://www.buffett-code.com/company/<CODE>': 'バフェットコード',
+  'https://shikiho.jp/tk/stock/info/<CODE>': '四季報オンライン',
+  'https://shikiho.jp/services/Query?SRC=shikiho/basic/year/base&code=<CODE>': '四季報オンライン：業績情報',
+  'https://kabutan.jp/stock/news?code=<CODE>': '株探：ニュース',
+  'https://www.sbisec.co.jp/ETGate/WPLETsiR001Control/WPLETsiR001Ilst10/getDetailOfStockPriceJP?OutSide=on&getFlg=on&stock_sec_code_mul=<CODE>&exchange_code=TKY': 'SBI証券',
+  'https://karauri.net/<CODE>/': 'karauri.net',
+  'https://textream.yahoo.co.jp/search?query=<CODE>': '株式掲示板 - textream'
+};
