70531c41a159919cc7d2af8e2f2b03f29580731c
westcide <nitchie@westcide.com>
2022-04-06 22:19:17 +0900

    Manifest V3で動くように直した

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
-// Copyright 2018 westcide.com All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
-// Add a listener to create the initial context menu items,
-// context menu items only need to be created at runtime.onInstalled
+importScripts('sites.js');
+
 chrome.runtime.onInstalled.addListener(function() {
   for (let key of Object.keys(kSites)) {
     chrome.contextMenus.create({
